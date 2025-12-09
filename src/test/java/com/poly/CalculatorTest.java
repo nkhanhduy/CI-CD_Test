d01e8af2c0f893cda78e31addf2465b04c1ad8cd
Nguyen Khanh Duy <khanhndts02168@gmail.com>
2025-12-09 14:00:08 +0700

    check

--- src/test/java/com/poly/CalculatorTest.java
+++ src/test/java/com/poly/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
 
     @Test
     public void testCong() {
-        Assert.assertEquals(calc.add(10, 20), 20);
+        Assert.assertEquals(calc.add(10, 20), 30);
     }
 
     @Test
